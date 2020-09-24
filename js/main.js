482ce582c7cfc025b416f33b7b3786d6e89b9c07
AbdulAziz77 <gerzeliyev@mail.ru>
2020-09-24 15:31:59 +0300

    fixed: hotel-slide-images & responsive for reviews

--- js/main.js
+++ js/main.js
@@ -22,10 +22,11 @@ var reviewsSlider = new Swiper('.reviews-slider', {
   
     // Navigation arrows
     navigation: {
-      nextEl: '.hotel-slider__button--next',
-      prevEl: '.hotel-slider__button--prev',
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
     },
-
+    
+    
     effect: 'fade',
 
     keyboard: {
