f675ed054fd4f55cfa7536ecbe69136c4983359d
AbdulAziz77 <gerzeliyev@mail.ru>
2020-10-01 17:40:36 +0300

    create: media mobile

--- js/main.js
+++ js/main.js
@@ -16,6 +16,17 @@ var hotelSlider = new Swiper('.hotel-slider', {
 
   });
 
+ 
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    console.log("Клик");
+    document
+      .querySelector(".navbar-bottom")
+      .classList.toggle("navbar-bottom--visible");
+  });
+
+
+
 var reviewsSlider = new Swiper('.reviews-slider', {
     // Optional parameters
     loop: true,
