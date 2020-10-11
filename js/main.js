3ca2e345e0df0a04f74eebd924059818afcba9a8
Abdul-Aziz77 <gerzeliyev@mail.ru>
2020-10-11 08:57:43 +0300

    craete: optimization

--- js/main.js
+++ js/main.js
@@ -99,5 +99,8 @@ var reviewsSlider = new Swiper('.reviews-slider', {
       },
     });
   });
+  $('[type="tel"]').each(function() {
+    $(this).mask("+7(999) 999-99-99")
+  }),
   AOS.init();
 });
